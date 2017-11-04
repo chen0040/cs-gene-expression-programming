8ecaa101e54b1d71c19ce80ab17d790a8e37737c
xschen <xs0040@gmail.com>
2017-11-04 12:24:47 +0800

    sample codes'

---
+++ cs-gene-expression-programming-samples/SpiralFitnessCase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TreeGP.Core.ProblemModels;
+
+namespace GEP.SpiralClassification
+{
+
+    public class SpiralFitnessCase : IGPFitnessCase
+    {
+        private double mX;
+        private double mY;
+        private int mLabel;
+        private int mComputedLabel;
+
+        public int ComputedLabel
+        {
+            get { return mComputedLabel; }
+        }
+
+        public int Label
+        {
+            get { return mLabel; }
+            set { mLabel = value; }
+        }
+
+        public double X
+        {
+            get { return mX; }
+            set { mX = value; }
+        }
+
+        public double Y
+        {
+            get { return mY; }
+            set { mY = value; }
+        }
+
+       
+
+        public void StoreOutput(object result, int program_index)
+        {
+            double dresult = (double)result;
+            if (dresult < 0.5)
+            {
+                mComputedLabel = -1;
+            }
+            else
+            {
+                mComputedLabel = 1;
+            }
+        }
+
+        public bool QueryInput(string variable_name, out object input)
+        {
+            input = 0;
+            if (variable_name == "X")
+            {
+                input = mX;
+                return true;
+            }
+            else if (variable_name == "Y")
+            {
+                input = mY;
+                return true;
+            }
+            return false;
+        }
+
+
+        public int GetInputCount()
+        {
+            return 2;
+        }
+
+
+    }
+}
